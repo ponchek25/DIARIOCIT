f2b388ffa3eaa04ddfe56928cbe4addc38a2dd0e
ponchek25 <125266736+ponchek25@users.noreply.github.com>
2024-11-12 21:16:51 -0300

    2

--- script.js
+++ script.js
@@ -3,40 +3,25 @@ $(document).ready(function() {
  
   function gerarQuote() {
     var quoteArray = [
-      '"Não desista até que você fique orgulhoso."',
-      '"Certas coisas valem a pena lutar."',
-      '"Não se force a ser feliz."',
-      '"Ame quando estiver pronto, e não sozinho."',
-      '"Permita a si mesmo se recuperar."',
-      '"Faça por você e não por eles."',
-      '"Seja seu próprio herói hoje."',
-      '"Suas decisões determinam seu destino."',
-      '"Seja a energia que você quer atrair."',
-      '"Todos que você conhecer pelo caminho estão lutando as próprias batalhas. Seja gentil sempre."',
-      '"Continue determinado."',
-      '"Dance como se niguém estivesse te olhando."',
-      '"Seja honesto com os seus desejos."',
-      '"Celebre as pequenas vitórias."',
-      '"Seu maior poder é ser você."',
-      '"Se você não amar a si mesmo como poderá amar outra pessoa?"',
-      '"Acredite em si mesmo e confie nos outros."',
-      '"Se abra as possibilidades."',
-      '"As coisas mais lindas em você vêm de dentro."',
-      '"Tudo é possível se você tiver coragem."',
-      '"A vida é uma grande aventura."',
-      '"São tempos difíceis que nos ensinam as mais valiosas lições."',
-      '"Você chegou até aqui, não desista agora."',
-      '"Nunca interrompa suas lágrimas."',
-      '"Você é mais forte do que pensa."',
-      '"Não deixe a vida te derrubar."',
-      '"Toda flor floresce em seu próprio tempo."',
-      '"Se tropeçar, faça disso parte da dança."'
+      '"A sociologia é um esporte de combate, a escola é violenta, a educação não é romântica como falam." Fabrízzia Santos',
+      '“Os jovens brancos precisam se reconhecer como atores dessa violência.”   Fabrízzia Santos',
+      '“Vida é dor, é sofrimento também” Beatriz Batista Silva',
+      '“Não é todo dia que tenho orgulho, de ser preta e deficiente, as vezes queria ser branca e poder entrar em todos os lugares sem nenhuns problemas, essa estrutura nos leva a querer isso, por isso devemos ter orgulho para nos mantermos de pé.” Beatriz Batista Silva',
+      '“Não podemos estar sozinhos e é essencial ter referência. Professores que sejam exemplos e referência.” Beatriz Batista Silva',
+      '“Quando a escola é de vidro (Rute Rocha), nós precisamos primeiro quebrar nosso vidro, não mudamos fora se não mudarmos dentro.” Silvia Mota',
+      '"Uma potência" Ângela Maria de Sousa Lima',
+      '“São questões que ferem o ECA, mandar prender o cabelo vai contra a identidade dos adolescentes. Vamos usar a lei ao nosso favor. Os estudantes fizeram um B.O. para os militares.” Silvia Mota',
+      '"O que se vê quando se olha?" Zuleika Bueno',
+      '"Se abrissemos as pessoas. Encontraríamos paisagens" Zuleika Bueno',
+      '"A plena cidadania do índio depende de sua interação com a sociedade nacional e do conhecimento, dos valores morais e costumes por ela adotados, a partir desta interação." Chiquinha Paresi',
+      '"Deixar claro quem você é e o que pretende." Paulo Stumpf',
+      '"O currículo do curso de História da Universidade é Branca" Marilyn Beloni'
     ];
     
    var randomQuote = quoteArray[Math.floor(Math.random() * quoteArray.length)];
     document.getElementById("quote").innerHTML = randomQuote;
     var by = " %23indiretasdobem via https://codepen.io/itska/full/PzwyxZ/";
-    document.getElementById("tweet").href = "https://twitter.com/intent/tweet?&text=" + randomQuote + by;
+    document.getElementById("tweet").href = "https://pos.uel.br/profsocio/" + randomQuote + by;
 }
  
 document.getElementById("quoteButton").onclick = gerarQuote;
